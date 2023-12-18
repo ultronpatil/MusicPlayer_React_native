d4a9af2f7f152083cb22cb9c3f32b2c60e518a64
Ultron Patil <ultronpatil@gmail.com>
2023-12-19 00:06:57 +0530

    created music player window basic

--- app/screens/Player.js
+++ app/screens/Player.js
@@ -1,21 +1,61 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Dimensions } from 'react-native';
+import Screen from '../componants/Screen';
+import Color from '../misc/Color';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Slider from '@react-native-community/slider';
+
+const { width } = Dimensions.get("window")
 
 const Player = () => {
     return (
-        <View style={styles.container}>
+        <Screen>
+            <View style={styles.container}>
 
-            <Text>Player</Text>
-        </View>
+                <Text style={styles.audioCount}>
+                    1 / 99
+                </Text>
+
+                <View style={styles.midBannerContainer}>
+                    <MaterialCommunityIcons name="music-circle" size={300} color={Color.ACTIVE_BG} />
+                </View>
+
+                <View style={styles.audioPlayerContainer}>
+                    <Text numberOfLines={1} style={styles.audioTitle}>song name</Text>
+                    <Slider
+                        style={{ width: width, height: 40 }}
+                        minimumValue={0}
+                        maximumValue={1}
+                        minimumTrackTintColor={Color.FONT_MEDIUM}
+                        maximumTrackTintColor={Color.ACTIVE_BG}
+                    />
+                </View>
+
+            </View>
+        </Screen>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center'
+    },
+    audioCount: {
+        textAlign: 'right',
+        padding: 15,
+        color: Color.FONT_LIGHT,
+        fontSize: 14,
+
+    },
+    midBannerContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    audioTitle: {
+        color: Color.FONT_MEDIUM,
+        fontSize: 16,
+        padding: 15
     }
 })
 
